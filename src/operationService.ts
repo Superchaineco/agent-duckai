9249f1b291e1acefe4bc248b8efe77607627faca
anotherdev <joseluismanco37@gmail.com>
2025-02-13 20:59:43 -0500

    feat: add enso transactions

---
+++ src/operationService.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+interface RouteParams {
+  fromAddress: string;
+  receiver: string;
+  spender: string;
+  amountIn: string;
+  tokenIn: string;
+  tokenOut: string;
+  routingStrategy: string;
+}
+
+export async function getOperationCalldata(params: RouteParams): Promise<any> {
+  const { fromAddress, receiver, spender, amountIn, tokenIn, tokenOut } =
+    params;
+  const url = `https://api.enso.finance/api/v1/shortcuts/route?chainId=10&fromAddress=${fromAddress}&receiver=${receiver}&spender=${spender}&amountIn=${amountIn}&tokenIn=${tokenIn}&tokenOut=${tokenOut}&routingStrategy=delegate`;
+
+  try {
+    const response = await axios.get(url, {
+        headers: {
+             Authorization: `Bearer 7d7c4bad-42b6-471f-b890-9912f8feb78b`
+        }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching route:', error);
+    throw error;
+  }
+}
